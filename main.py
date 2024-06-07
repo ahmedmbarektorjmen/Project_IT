84ba4aacee11b63531706e83184b5d1d0363803b
ahmedmbarektorjmen <mbarekahmed0000@outlook.com>
2024-06-08 00:24:20 +0100

    'v2.0.1'

--- main.py
+++ main.py
@@ -267,4 +267,4 @@ async def home(request: Request, url: str):
 # running the web app:
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run('main:app', host='0.0.0.0', port=8000,reload=True)
+    uvicorn.run('main:app', host='0.0.0.0', port=80,reload=True)
